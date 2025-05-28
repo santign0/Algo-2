643243b82f81010f558c8d684208131ab45bd927
santign0 <142273659+santign0@users.noreply.github.com>
2025-05-28 14:09:03 -0300

    taller 4

---
+++ ABB.java
@@ -0,0 +1,110 @@
+package aed;
+
+import java.util.*;
+
+// Todos los tipos de datos "Comparables" tienen el método compareTo()
+// elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2           5.compareTo(7) < 0 == true
+public class ABB<T extends Comparable<T>> implements Conjunto<T> {
+    private Nodo raiz;
+    private int cardinal;
+
+    private class Nodo {
+        T valor;
+        Nodo izq;
+        Nodo der;
+        Nodo padre;
+        
+        Nodo(T v) {this.valor = v; }
+    }
+
+    public ABB() {
+        raiz = null;
+        cardinal = 0;
+    }
+
+    public int cardinal() {
+        return cardinal;
+    }
+
+    public T minimo(){
+        Nodo actual = raiz;
+        while (actual.izq != null) {
+            actual = actual.izq;
+        }return actual.valor;
+    }
+
+    public T maximo(){
+        Nodo actual = raiz;
+        while (actual.der != null) {
+            actual = actual.der;
+        }return actual.valor;
+    }
+
+    private Nodo buscarPadre(Nodo raiz , T e){
+        Nodo actual = raiz;
+        if (e.compareTo(actual.valor) > 0){
+            if (actual.der == null){
+                return actual;
+            }else{
+                return buscarPadre(actual.der, e);
+            }
+        }else if(e.compareTo(actual.valor) < 0){
+            if (actual.izq == null){
+                return actual;
+            }else{
+                return buscarPadre(actual.izq, e);
+            }
+        }else{
+            return actual;
+        }
+    }
+    public void insertar(T elem){
+        Nodo e = new Nodo(elem);
+        Nodo actual = raiz;
+        if (raiz != null){
+            if (buscarPadre(raiz, elem)== actual && elem.compareTo(actual.valor) < 0){
+                actual.izq = e;
+                e.padre = actual;
+            }else if(buscarPadre(raiz, elem) == actual && elem.compareTo(actual.valor) > 0){
+                actual.der = e;
+                e.padre = actual;
+            }cardinal ++;
+        }
+    }
+
+
+    public boolean pertenece(T elem){
+        Nodo actual = raiz;
+        if (actual.valor == null){
+            return false;
+        }
+        else if (actual.valor == elem){
+            return true;
+        }
+    }
+
+    public void eliminar(T elem){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public String toString(){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    private class ABB_Iterador implements Iterador<T> {
+        private Nodo _actual;
+
+        public boolean haySiguiente() {            
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    
+        public T siguiente() {
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    }
+
+    public Iterador<T> iterador() {
+        return new ABB_Iterador();
+    }
+
+}
